83863c98b057378f49b47962672ba598dd3dca6d
laceous <laceous@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-23 08:42:30 +0000

    removing 0.1
    git-svn-id: https://plugins.svn.wordpress.org/invalidate-logged-out-cookies/tags/0.1@128687 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++
@@ -1,5 +0,0 @@
-<?php
-if ( defined('ABSPATH') && defined('WP_UNINSTALL_PLUGIN') ) {
-	$GLOBALS['wpdb']->query( 'DROP TABLE ' . $GLOBALS['wpdb']->prefix.'invalidated_cookies' );
-	delete_option('invalidated_cookies_db_ver');
-}
